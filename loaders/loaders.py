2771460fa97d0560f3dbb8eab43846c31d31e3b8
Jorrit Ypenga <jorrit@sowiso.com>
2022-04-13 11:47:07 +0200

    merged geometry.py with loaders.py

--- loaders/loaders.py
+++ loaders/loaders.py
@@ -13,11 +13,40 @@ import matplotlib.pyplot as plt
 
 from PIL import Image
 from pprint import pprint
-from .geometry import viewpoint_to_pixels, reindex
 
 from skimage import io, transform
 from torch.utils.data import Dataset
 from torchvision import transforms
+
+
+# helper functions
+def pixel_to_viewpoint(pixel, image_width=8000, dtype=int):
+    """
+    Convert width in pixels to viewpoint in degrees
+    """
+    return dtype(360 * pixel / image_width)
+
+
+def viewpoint_to_pixels(viewpoint, image_width=8000, dtype=int):
+    """
+    Convert viewpoint in degrees to width in pixels
+    """
+    return dtype(viewpoint / 360 * image_width)
+
+
+def reindex(index, max):
+    """
+    Reindex out-of-bounds indices
+    """
+    if not index:
+        return index
+
+    if index < 0:
+        index += max
+    elif index > max:
+        index -= max
+
+    return index
 
 
 class PanoramaImage(object):
